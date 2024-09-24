bdacb8259c4e6228b299603a79768d9f1e398baa
Abdulmujeeb <olamilekanabdulmujeeb15@gmail.com>
2024-09-23 23:29:15 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// var whatisyourname = prompt
+// alert('Welcome ' + whatisyourname)
+function abdul() {
+  let input = document.querySelector(".input").value;
+  if (input.length <= 0) {
+    return alert("pls input something valid");
+  }
+  const headings = document.createElement("li");
+  const todos = document.querySelector(".todos");
+  headings.style.fontSize = "20px";
+  headings.innerHTML = input;
+  todos.appendChild(headings);
+  document.querySelector(".input").value = "";
+}
